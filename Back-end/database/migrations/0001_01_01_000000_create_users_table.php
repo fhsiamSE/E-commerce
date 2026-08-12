daaedcb77d1e0f5aeb51658f57cb5881b757749b
fhsiamSE <faisalhossainsiam@gmail.com>
2026-08-12 16:07:11 +0600

    remove email verification field from table

--- Back-end/database/migrations/0001_01_01_000000_create_users_table.php
+++ Back-end/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('image')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address')->nullable();
             $table->string('phone_number')->nullable();            
